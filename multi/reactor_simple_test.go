49313f04c6bd32fc83ab78b62a10b0e5d1e7b45b
Andre Soares <andregsilv@gmail.com>
2023-08-17 19:18:51 -0300

    two requests

--- multi/reactor_simple_test.go
+++ multi/reactor_simple_test.go
@@ -27,7 +27,6 @@ func TestSimpleReactor_HappyPath_SingleRequest(t *testing.T) {
 		Header:  []byte(gofakeit.Lexify("????")),
 		Payload: []byte(gofakeit.BuzzWord()),
 	}
-	gofakeit.Struct(&fakeRequest)
 	fakeResponse := hsmlib.Packet{
 		Header:  []byte(gofakeit.Lexify("????")),
 		Payload: []byte(gofakeit.BuzzWord()),
@@ -57,6 +56,70 @@ func TestSimpleReactor_HappyPath_SingleRequest(t *testing.T) {
 	resp, err := reactor.Post(fakeRequest.Payload)
 	require.NoError(t, err)
 	assert.Equal(t, fakeResponse.Payload, resp)
+
+	reactor.Wait()
+}
+
+func TestSimpleReactor_HappyPath_TwoRequests(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockIDManager := mocks.NewMockIDManager(ctrl)
+	mockPacketStream := mocks.NewMockPacketStream(ctrl)
+	mockCloser := mocks.NewMockCloser(ctrl)
+
+	fakeRequest1 := hsmlib.Packet{
+		Header:  []byte(gofakeit.Lexify("????")),
+		Payload: []byte(gofakeit.BuzzWord()),
+	}
+	fakeResponse1 := hsmlib.Packet{
+		Header:  []byte(gofakeit.Lexify("????")),
+		Payload: []byte(gofakeit.BuzzWord()),
+	}
+	fakeResponseChan1 := make(chan []byte)
+	fakeRequest2 := hsmlib.Packet{
+		Header:  []byte(gofakeit.Lexify("????")),
+		Payload: []byte(gofakeit.BuzzWord()),
+	}
+	fakeResponse2 := hsmlib.Packet{
+		Header:  []byte(gofakeit.Lexify("????")),
+		Payload: []byte(gofakeit.BuzzWord()),
+	}
+	fakeResponseChan2 := make(chan []byte)
+
+	mockIDManager.EXPECT().NewID().Return(fakeRequest1.Header, fakeResponseChan1)
+	mockIDManager.EXPECT().NewID().Return(fakeRequest2.Header, fakeResponseChan2)
+	mockPacketStream.EXPECT().SendPacket(fakeRequest1).Return(nil)
+	mockPacketStream.EXPECT().ReceivePacket().Do(func() {
+		time.Sleep(10 * time.Millisecond)
+	}).Return(fakeResponse1, nil)
+	mockPacketStream.EXPECT().SendPacket(fakeRequest2).Return(nil)
+	mockPacketStream.EXPECT().ReceivePacket().Do(func() {
+		time.Sleep(10 * time.Millisecond)
+	}).Return(fakeResponse2, nil)
+	mockPacketStream.EXPECT().ReceivePacket().Return(hsmlib.Packet{}, io.EOF)
+	mockIDManager.EXPECT().FindChannel(fakeResponse1.Header).Return(fakeResponseChan1, true)
+	mockIDManager.EXPECT().FindChannel(fakeResponse2.Header).Return(fakeResponseChan2, true)
+	mockCloser.EXPECT().Close()
+
+	reactor := NewSimpleReactor(nil)
+	require.NotNil(t, reactor)
+	require.NotEmpty(t, reactor)
+
+	reactor.idManager = mockIDManager
+	reactor.target = mockPacketStream
+	reactor.connectionCloser = mockCloser
+
+	err := reactor.Start()
+	require.NoError(t, err)
+
+	resp, err := reactor.Post(fakeRequest1.Payload)
+	require.NoError(t, err)
+	assert.Equal(t, fakeResponse1.Payload, resp)
+
+	resp, err = reactor.Post(fakeRequest2.Payload)
+	require.NoError(t, err)
+	assert.Equal(t, fakeResponse2.Payload, resp)
 
 	reactor.Wait()
 }
